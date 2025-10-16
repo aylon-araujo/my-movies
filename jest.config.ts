bb86669168b3eb714820255c4b85ab857797c6e5
Aylon Araújo <aylonmsa@gmail.com>
2025-10-16 14:18:56 -0300

    test: configure initial Jest setup with React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    // CSS Modules
+    '\\.module\\.scss$': 'identity-obj-proxy',
+    // Assets (images, etc)
+    '\\.(css|scss|png|jpg|jpeg|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'lcov'],
+};
+
+export default config;
