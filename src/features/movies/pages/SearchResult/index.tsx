6d3a6b0632b305e67a44ba864bbe916d73350f94
Aylon Araújo <aylonmsa@gmail.com>
2025-10-20 09:26:31 -0300

    style: general layout and spacing adjustments

--- src/features/movies/pages/SearchResult/index.tsx
+++ src/features/movies/pages/SearchResult/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { FloatingButton, MovieGrid } from "@shared/components";
+import { FloatingButton, If, MovieGrid } from "@shared/components";
+import { pluralize } from "@shared/utils/pluralize";
 
-import { useMovieList } from '../../hooks/useMovieList';
-import styles from './SearchPage.module.scss'
+import { useMovieList } from "../../hooks/useMovieList";
+import styles from "./SearchPage.module.scss";
 
 const SearchResult: React.FC = () => {
-  const { 
-    movies, 
-    isLoading, 
-    error, 
-    currentPage, 
-    totalPages, 
+  const {
+    movies,
+    isLoading,
+    error,
+    currentPage,
+    totalPages,
     goToPage,
     currentQuery,
     isSearch,
@@ -26,14 +27,21 @@ const SearchResult: React.FC = () => {
   }
   return (
     <div className={styles.searchPageContainer}>
-      <h1 className={styles.searchTitle}>
-        Resultados para: "
-        <span className={styles.queryHighlight}>
-          {decodeURIComponent(currentQuery)}
-        </span>
-        "
-      </h1>
-      
+      <div className={styles.searchPageHead}>
+        <h1 className={styles.searchTitle}>
+          Resultados para:{" "}
+          <span className={styles.queryHighlight}>
+            "{decodeURIComponent(currentQuery)}"
+          </span>
+        </h1>
+        <If condition={!!movies.length}>
+          <span>
+            {pluralize(movies.length > 1, "Encontrado")} {movies.length}{" "}
+            {pluralize(movies.length > 1, "filme")}
+          </span>
+        </If>
+      </div>
+
       <MovieGrid
         movies={movies}
         isLoading={isLoading}
